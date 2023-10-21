e95cda5364929bdaf29163e4bb8600510cc60f4a
bibek <bibekdahal479@gmail.com>
2023-10-21 11:31:26 +0545

    localization left

--- dairy/settings.py
+++ dairy/settings.py
@@ -88,24 +88,27 @@ WSGI_APPLICATION = 'dairy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+if bool(int(os.environ.get('PRODUCTION',0))):
+
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("DB_ENGINE"),
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_ROOT_USER"),
+            "PASSWORD": os.environ.get("DB_ROOT_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT")
+        }
     }
-}
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.environ.get("DB_ENGINE"),
-#         "NAME": os.environ.get("DB_NAME"),
-#         "USER": os.environ.get("DB_ROOT_USER"),
-#         "PASSWORD": os.environ.get("DB_ROOT_PASSWORD"),
-#         "HOST": os.environ.get("DB_HOST"),
-#         "PORT": os.environ.get("DB_PORT")
-#     }
-# }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
