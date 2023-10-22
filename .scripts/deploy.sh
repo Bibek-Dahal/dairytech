3023828ec59f92c8be2e96b013ad2b95f18be744
bibek <bibekdahal479@gmail.com>
2023-10-22 15:42:14 +0545

    modified .sh branch

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "Deployment started ..."
 
 # Pull the latest version of the app
-git pull origin master
+git pull origin main
 echo "New changes copied to server !"
 
 # Activate Virtual Env
