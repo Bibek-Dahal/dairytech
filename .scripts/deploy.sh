75ff1e046d2a20acbb19127017ae028ad26fc3aa
bibek <bibekdahal479@gmail.com>
2023-10-22 15:23:30 +0545

    added CICD

---
+++ .scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+# Activate Virtual Env
+source source /home/merodairy/virtualenv/dairyapp/dairytech/3.9/bin/activate
+echo "Virtual env 'mb' Activated !"
+
+echo "Installing Dependencies..."
+pip install -r requirements.txt --no-input
+
+echo "Serving Static Files..."
+python manage.py collectstatic --noinput
+
+echo "Running Database migration"
+python manage.py makemigrations
+python manage.py migrate
+
+# Deactivate Virtual Env
+deactivate
+echo "Virtual env 'mb' Deactivated !"
+
+# Reloading Application So New Changes could reflect on website
+# pushd miniblog
+# touch wsgi.py
+# popd
+
+echo "Deployment Finished!"
